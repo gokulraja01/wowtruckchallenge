4cb75c59303d6d4df15511878f4774414812e175
gokulraja01 <gokulraja01@gmail.com>
2016-09-06 12:50:00 +0530

    code alignment

--- frontend/views/mark/_form.php
+++ frontend/views/mark/_form.php
@@ -17,31 +17,29 @@ use app\models\Subject;
 		  </header>
 		  <div class="panel-body">
 			  <?php $form = ActiveForm::begin([
-					'options' => ['class' => 'form-horizontal', 'id' => 'create_mark'],
-					'fieldConfig' => ['options' => ['tag'=>false] ]				
-					]); ?>
+				'options' => ['class' => 'form-horizontal', 'id' => 'create_mark'],
+				'fieldConfig' => ['options' => ['tag'=>false] ]				
+				]); ?>
 					
-					<div class="form-group">
-					  <label class="col-sm-2 control-label">Student Name</label>
-					  <div class="col-sm-10">
-							<?php $items = ArrayHelper::map(Student::find()->all(), 'id', 'student_name'); ?>
-							<?= $form->field($model, 'student_id')->dropDownList($items,['prompt' => 'Select Student'],['class' => 'form-control m-bot15'])->label(false); ?>
-					  </div>
-					 </div> 
-					                                                           
-					<?php $subject = ArrayHelper::map(Subject::find()->all(), 'subject_id', 'subject_name'); 
-					foreach($subject as $key => $subject): ?>
-					
-					<div class="form-group">
-					  <label class="col-sm-2 control-label"><?php echo $subject; ?></label>
-					  <div class="col-sm-10">							  
-						   <?= $form->field($model, "mark[".$key."]")->textInput(['maxlength' => true,'class' => 'form-control'])->label(false); ?>
-					  </div>
-					</div>
-					                          
-					<?php endforeach; ?>   
-					 <a href="<?php echo Yii::$app->homeUrl;?>mark" class="btn btn-primary">Back</a>
-					 <?= Html::submitButton($model->isNewRecord ? 'Enter' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>                  				               <?php ActiveForm::end(); ?>
+				<div class="form-group">
+				  <label class="col-sm-2 control-label">Student Name</label>
+				  <div class="col-sm-10">
+					<?php $items = ArrayHelper::map(Student::find()->all(), 'id', 'student_name'); ?>
+					<?= $form->field($model, 'student_id')->dropDownList($items,['prompt' => 'Select Student'],['class' => 'form-control m-bot15'])->label(false); ?>
+				  </div>
+				 </div> 
+				                                                           
+				<?php $subject = ArrayHelper::map(Subject::find()->all(), 'subject_id', 'subject_name'); 
+				foreach($subject as $key => $subject): ?>
+				<div class="form-group">
+				  <label class="col-sm-2 control-label"><?php echo $subject; ?></label>
+				  <div class="col-sm-10">							  
+					   <?= $form->field($model, "mark[".$key."]")->textInput(['maxlength' => true,'class' => 'form-control'])->label(false); ?>
+				  </div>
+				</div>                        
+				<?php endforeach; ?>   
+				 <a href="<?php echo Yii::$app->homeUrl;?>mark" class="btn btn-primary">Back</a>
+				 <?= Html::submitButton($model->isNewRecord ? 'Enter' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>                  				               <?php ActiveForm::end(); ?>
 		  </div>
 	  </section>
   </div>
