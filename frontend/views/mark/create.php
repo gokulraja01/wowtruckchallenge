b3457f74dd97449eee86bbecbea5c7a584802ba0
gokulraja01 <gokulraja01@gmail.com>
2016-09-06 12:50:42 +0530

    code alignment

--- frontend/views/mark/create.php
+++ frontend/views/mark/create.php
@@ -11,17 +11,17 @@ use yii\widgets\ActiveForm;
 <!--main content start-->
 <section id="main-content">
   <section class="wrapper">
-	  <div class="row">
-			<div class="col-lg-12">
-				<h3 class="page-header"><i class="fa fa-table"></i> Enter Marks</h3>
-				<ol class="breadcrumb">
-					<li><i class="fa fa-home"></i><a href="#">Home</a></li>
-					<li><i class="fa fa-table"></i>Marks</li>
-					<li><i class="fa fa-table"></i>Enter Marks</li>
-				</ol>
-			</div>
-		</div>	
-	  <?= $this->render('_form', ['model' => $model,]) ?>	  
+	<div class="row">
+		<div class="col-lg-12">
+			<h3 class="page-header"><i class="fa fa-table"></i> Enter Marks</h3>
+			<ol class="breadcrumb">
+				<li><i class="fa fa-home"></i><a href="#">Home</a></li>
+				<li><i class="fa fa-table"></i>Marks</li>
+				<li><i class="fa fa-table"></i>Enter Marks</li>
+			</ol>
+		</div>
+	</div>	
+	<?= $this->render('_form', ['model' => $model,]) ?>	  
    </section>
 </section>
 <!--main content end-->
