37c401c7c51617625eb281aa895f590043588c0b
gokulraja01 <gokulraja01@gmail.com>
2016-09-06 14:36:07 +0530

    code alignment

--- frontend/views/student/create.php
+++ frontend/views/student/create.php
@@ -10,23 +10,23 @@ use yii\widgets\ActiveForm;
 
 <!--main content start-->
 <section id="main-content">
-  <section class="wrapper">
-  <div class="row">
-		<div class="col-lg-12">
-			<h3 class="page-header"><i class="fa fa-user"></i> Add Students</h3>
-			<ol class="breadcrumb">
-				<li><i class="fa fa-home"></i><a href="#">Home</a></li>
-				<li><i class="fa fa-users"></i>Students</li>
-				<li><i class="fa fa-user"></i>Add Student</li>
-			</ol>
+  	<section class="wrapper">
+  		<div class="row">
+			<div class="col-lg-12">
+				<h3 class="page-header"><i class="fa fa-user"></i> Add Students</h3>
+				<ol class="breadcrumb">
+					<li><i class="fa fa-home"></i><a href="#">Home</a></li>
+					<li><i class="fa fa-users"></i>Students</li>
+					<li><i class="fa fa-user"></i>Add Student</li>
+				</ol>
+			</div>
 		</div>
-	</div>
 	
-	  <?= $this->render('_form', [
-		'model' => $model,
-	  ]) ?>
+		<?= $this->render('_form', [
+			'model' => $model,
+		]) ?>
 	  
-</section>
+	</section>
 </section>
 <!--main content end-->
   
