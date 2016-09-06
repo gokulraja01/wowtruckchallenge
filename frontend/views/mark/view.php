7e25ef3fe8800c682505a0c7d89ed4947aa99594
gokulraja01 <gokulraja01@gmail.com>
2016-09-06 14:49:12 +0530

    code alignment

--- frontend/views/mark/view.php
+++ frontend/views/mark/view.php
@@ -8,59 +8,59 @@ use app\models\Student;
 ?>
 <!--main content start-->
 <section id="main-content">
-  <section class="wrapper">
-  <div class="row">
-		<div class="col-lg-12">
-			<h3 class="page-header"><i class="fa fa-table"></i> View Marks</h3>
-			<ol class="breadcrumb">
-				<li><i class="fa fa-home"></i><a href="index.html">Home</a></li>
-				<li><i class="fa fa-table"></i>Marks</li>
-				<li><i class="fa fa-table"></i>View Mark</li>
-			</ol>
+  	<section class="wrapper">
+	  	<div class="row">
+			<div class="col-lg-12">
+				<h3 class="page-header"><i class="fa fa-table"></i> View Marks</h3>
+				<ol class="breadcrumb">
+					<li><i class="fa fa-home"></i><a href="index.html">Home</a></li>
+					<li><i class="fa fa-table"></i>Marks</li>
+					<li><i class="fa fa-table"></i>View Mark</li>
+				</ol>
+			</div>
 		</div>
-	</div>
 	
-	<?php if(Yii::$app->session->getFlash('success')): ?>
-	<div class="alert alert-success fade in">
-	  <button data-dismiss="alert" class="close close-sm" type="button">
-		<i class="fa fa-times"></i>
-	  </button>
-	  <strong>Success!</strong> <?php echo Yii::$app->session->getFlash('success'); ?>
-	</div>
-	<?php endif; ?>
+		<?php if(Yii::$app->session->getFlash('success')): ?>
+			<div class="alert alert-success fade in">
+			  <button data-dismiss="alert" class="close close-sm" type="button">
+				<i class="fa fa-times"></i>
+			  </button>
+			  <strong>Success!</strong> <?php echo Yii::$app->session->getFlash('success'); ?>
+			</div>
+		<?php endif; ?>
 	
-	  <div class="row">
-		  <div class="col-lg-12">
-			  <section class="panel">
-				  <header class="panel-heading">                             
-				  </header>
-				  <div class="panel-body">
-					  <form class="form-horizontal " method="get">
-						   <div class="form-group">
-							  <label class="col-lg-2 control-label">Subject Name</label>
-							  <div class="col-lg-10">
-								<?php $student = Student::find()->where(['id' => $model->student_id])->all();?>	
-								  <p class="form-control-static"><?php echo $student[0]['student_name']; ?></p>
-							  </div>
-						  </div>
-						   <?php $mark = json_decode($model->mark);								   
-						  foreach ($mark as $key => $mark): ?>								  
-							  <div class="form-group">
-							  <?php $subject = Subject::find()->where(['subject_id' => $key])->all();?>	
-								  <label class="col-lg-2 control-label"><?php echo $subject[0]['subject_name']; ?></label>
+		<div class="row">
+			<div class="col-lg-12">
+				<section class="panel">
+					<header class="panel-heading">                             
+					</header>
+					<div class="panel-body">
+						<form class="form-horizontal " method="get">
+							<div class="form-group">
+								  <label class="col-lg-2 control-label">Subject Name</label>
 								  <div class="col-lg-10">
-									  <p class="form-control-static"><?php echo $mark; ?></p>
+									<?php $student = Student::find()->where(['id' => $model->student_id])->all();?>	
+									  <p class="form-control-static"><?php echo $student[0]['student_name']; ?></p>
 								  </div>
 							</div>
-						  <?php endforeach; ?>
-															
-						  <a href="<?php echo Yii::$app->homeUrl;?>mark" class="btn btn-primary">Back</a>
-					  </form>
-				  </div>
-			  </section>
-		  </div>
-	  </div>
-</section>
+							<?php $mark = json_decode($model->mark);								   
+							foreach ($mark as $key => $mark): ?>								  
+								<div class="form-group">
+								  	<?php $subject = Subject::find()->where(['subject_id' => $key])->all();?>	
+									<label class="col-lg-2 control-label"><?php echo $subject[0]['subject_name']; ?></label>
+									<div class="col-lg-10">
+										<p class="form-control-static"><?php echo $mark; ?></p>
+									</div>
+								</div>
+							<?php endforeach; ?>
+																
+							<a href="<?php echo Yii::$app->homeUrl;?>mark" class="btn btn-primary">Back</a>
+						</form>
+					</div>
+				</section>
+			</div>
+		</div>
+	</section>
 </section>
 <!--main content end-->
   
