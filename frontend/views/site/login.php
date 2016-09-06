9cf8d831aa8fef64e940ae7e19fd914bb741b8ad
gokulraja01 <gokulraja01@gmail.com>
2016-09-06 14:45:49 +0530

    code alignment

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -9,22 +9,23 @@ use yii\bootstrap\ActiveForm;
 
 ?>
 <?php $form = ActiveForm::begin([
-		'id' => 'login-form', 
-		'options' => ['class' => 'login-form'],		
-					'fieldConfig' => [                          
-						'options' => [
-							'tag'=>false
-						]
-					]		
+			'id' => 'login-form', 
+			'options' => 
+			['class' => 'login-form'],		
+			'fieldConfig' => [                          
+				'options' => [
+					'tag'=>false
+				]
+			]		
 		]); ?>        
         <div class="login-wrap">
             <p class="login-img"><i class="icon_lock_alt"></i></p>
             <div class="form-group">                           
-			  <?= $form->field($model, 'username')->textInput(['autofocus' => true,'class' => 'form-control','placeholder' => 'Username'])->label(false); ?>  
+			 <?= $form->field($model, 'username')->textInput(['autofocus' => true,'class' => 'form-control','placeholder' => 'Username'])->label(false); ?>  
             </div>
             <div class="form-group">                               
-				<?= $form->field($model, 'password')->passwordInput(['class' => 'form-control','placeholder' => 'Password'])->label(false); ?>
+			<?= $form->field($model, 'password')->passwordInput(['class' => 'form-control','placeholder' => 'Password'])->label(false); ?>
             </div>     
-			<?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-lg btn-block', 'name' => 'login-button']) ?>
+	    <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-lg btn-block', 'name' => 'login-button']) ?>
         </div>
 <?php ActiveForm::end(); ?>
