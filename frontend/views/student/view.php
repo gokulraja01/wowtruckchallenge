7c43aa3aa85d5843546af5d5be74cb4f5ccfdc63
gokulraja01 <gokulraja01@gmail.com>
2016-09-06 14:43:56 +0530

    code alignment

--- frontend/views/student/view.php
+++ frontend/views/student/view.php
@@ -1,58 +1,58 @@
 <!--main content start-->
-      <section id="main-content">
-          <section class="wrapper">
-		  <div class="row">
-				<div class="col-lg-12">
-					<h3 class="page-header"><i class="fa fa-user"></i> View Student</h3>
-					<ol class="breadcrumb">
-						<li><i class="fa fa-home"></i><a href="index.html">Home</a></li>
-						<li><i class="fa fa-users"></i>Students</li>
-						<li><i class="fa fa-user"></i>View Student</li>
-					</ol>
-				</div>
+<section id="main-content">
+  	<section class="wrapper">
+	  	<div class="row">
+			<div class="col-lg-12">
+				<h3 class="page-header"><i class="fa fa-user"></i> View Student</h3>
+				<ol class="breadcrumb">
+					<li><i class="fa fa-home"></i><a href="index.html">Home</a></li>
+					<li><i class="fa fa-users"></i>Students</li>
+					<li><i class="fa fa-user"></i>View Student</li>
+				</ol>
 			</div>
-			
-			<?php if(Yii::$app->session->getFlash('success')): ?>
+		</div>
+		
+		<?php if(Yii::$app->session->getFlash('success')): ?>
 			<div class="alert alert-success fade in">
 			  <button data-dismiss="alert" class="close close-sm" type="button">
 				<i class="fa fa-times"></i>
 			  </button>
 			  <strong>Success!</strong> <?php echo Yii::$app->session->getFlash('success'); ?>
 			</div>
-			<?php endif; ?>
-			
-              <div class="row">
-                  <div class="col-lg-12">
-                      <section class="panel">
-                          <header class="panel-heading">                             
-                          </header>
-                          <div class="panel-body">
-                              <form class="form-horizontal " method="get">
-                                   <div class="form-group">
-                                      <label class="col-lg-2 control-label">Student Name</label>
-                                      <div class="col-lg-10">
-                                          <p class="form-control-static"><?php echo $model->student_name; ?></p>
-                                      </div>
-                                  </div>
-                                  <div class="form-group">
-                                      <label class="col-lg-2 control-label">Father Name</label>
-                                      <div class="col-lg-10">
-                                          <p class="form-control-static"><?php echo $model->father_name; ?></p>
-                                      </div>
-                                  </div>
-                                  <div class="form-group">
-                                      <label class="col-lg-2 control-label">Email</label>
-                                      <div class="col-lg-10">
-                                          <p class="form-control-static"><?php echo $model->email; ?></p>
-                                      </div>
-                                  </div>
-                                  <a href="<?php echo Yii::$app->homeUrl;?>student" class="btn btn-primary">Back</a>
-                              </form>
-                          </div>
-                      </section>
-                  </div>
-              </div>
-    </section>
-   </section>
-      <!--main content end-->
+		<?php endif; ?>
+		
+	      	<div class="row">
+	          <div class="col-lg-12">
+	              <section class="panel">
+	                  <header class="panel-heading">                             
+	                  </header>
+	                  <div class="panel-body">
+	                      <form class="form-horizontal " method="get">
+	                           <div class="form-group">
+	                              <label class="col-lg-2 control-label">Student Name</label>
+	                              <div class="col-lg-10">
+	                                  <p class="form-control-static"><?php echo $model->student_name; ?></p>
+	                              </div>
+	                          </div>
+	                          <div class="form-group">
+	                              <label class="col-lg-2 control-label">Father Name</label>
+	                              <div class="col-lg-10">
+	                                  <p class="form-control-static"><?php echo $model->father_name; ?></p>
+	                              </div>
+	                          </div>
+	                          <div class="form-group">
+	                              <label class="col-lg-2 control-label">Email</label>
+	                              <div class="col-lg-10">
+	                                  <p class="form-control-static"><?php echo $model->email; ?></p>
+	                              </div>
+	                          </div>
+	                          <a href="<?php echo Yii::$app->homeUrl;?>student" class="btn btn-primary">Back</a>
+	                      </form>
+	                  </div>
+	              </section>
+	          </div>
+	      </div>
+	</section>
+</section>
+<!--main content end-->
   
